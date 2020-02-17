c5fc24aa7a20e39d16c680a4287e3f4cd55eb6b0
saint-yellow <saint-yellow@hotmail.com>
2020-02-17 16:10:15 +0800

    添加走棋逻辑

--- js/game.js
+++ js/game.js
@@ -1,49 +1,44 @@
-var X = ["A", "B", "C", "D", "E", "F", "G", "H"];
-var Y = [1, 2, 3, 4, 5, 6, 7, 8];
+// 棋盘的横坐标轴
+var xAxis = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+// 棋盘的纵坐标轴
+var yAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// 获取棋子的位置, 即棋子所处的网格的ID
+function getPosition(chess) {
+    var gridId = $(chess).parent().attr("id");
+    var x = gridId[0];
+    var y = parseInt(gridId[1]);
+
+    return [x, y];
+}
+
 
 $(".chess.pawn").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
-    
-    if ($(this).hasClass("black")) {
-
-    } else {
-
-    }
+    var position = getPosition(this);
 });
 
 $(".chess.king").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
+    var position = getPosition(this);
 
 });
 
 $(".chess.queen").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
+    var position = getPosition(this);
 
 });
 
 $(".chess.bishop").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
+    var position = getPosition(this);
 
 });
 
 $(".chess.knight").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
+    var position = getPosition(this);
 
 });
 
 $(".chess.rook").click(function () {
-    var id = $(this).parent().attr("id");
-    var x = id[0];
-    var y = parseInt(id[1]);
+    var position = getPosition(this);
 
 });
